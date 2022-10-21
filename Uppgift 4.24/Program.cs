4acc8daec5cc12f17a1ba24a998412bb8a86dce6
TheBlueSoul5188 <gujo04002@utb.vaxjo.se>
2022-10-21 11:13:29 +0200

    Add project files.

---
+++ Uppgift 4.24/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Uppgift_4_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välj ett heltal");
+            int tal1 = int.Parse(Console.ReadLine());
+            int störst = tal1;
+            string svar = "";
+            while (svar != "n")
+            {
+                Console.WriteLine("Vill du skriva in ett nytt heltal(Ja/nej)");
+                string svar2 = Console.ReadLine();
+                while (svar2 != "n")
+                {
+                    break;
+                    if (störst > tal1)
+                    {
+                        Console.WriteLine(störst);
+                    }
+                }    
+
+            }
+           
+        }
+    }
+}
